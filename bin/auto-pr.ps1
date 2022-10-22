878c69127e223b88436f6cac26dff139262af914
ciffelia <mc.prince.0203@gmail.com>
2022-10-22 17:49:49 +0900

    Fix branch name in auto-pr.ps1

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "ciffelia/ScoopBucket:main"
+    [String]$upstream = "ciffelia/ScoopBucket:master"
 )
 
 if(!$env:SCOOP_HOME) { $env:SCOOP_HOME = Resolve-Path (scoop prefix scoop) }
